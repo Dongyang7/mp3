b0bbff8f9d28ea17a684f5a63a05a64a0e4ce48a
Dongyang7 <dl7@illinois.edu>
2016-10-17 19:05:06 -0500

    Add files via upload

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,19 @@
 var app = angular.module('mp3',['ngRoute']);
+//var ascend = document.getElementById('sort').checked;
 
 app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/list",{
+        templateUrl : "partials/list.html",
+        controller:"lController"
+    })
+    .when("/details/:rank",{
+        templateUrl : "partials/details.html",
+        controller:"dController"
+    })
+    .when("/gallery",{
+        templateUrl : "partials/gallery.html",
+        controller:"gController"
+    });
 
-})
+});
--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,17 @@
 /* Sample Controller */
+app.controller('dController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $http.get("./data/imdb250.json").then(function(response){
+        $scope.info=response.data;
+    });
+    $scope.ranking=parseInt($routeParams.rank);
+}]);
+app.controller('lController', ['$scope', '$http', function($scope, $http){
+    $http.get("./data/imdb250.json").then(function(response){
+        $scope.info=response.data;
+    });
+}]);
+app.controller('gController', ['$scope', '$http', function($scope, $http){
+    $http.get("./data/imdb250.json").then(function(response){
+        $scope.info=response.data;
+    });
+}]);
